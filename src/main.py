da22fa76f4456b79557121a715f6da736c6c1517
leonard-vlaicu <vlaicu.leonard@yahoo.com>
2023-08-26 02:13:52 +0300

    debuging purposes on main

--- src/main.py
+++ src/main.py
@@ -1,13 +1,30 @@
 from core.scraper import Scraper
-from core.parser import Parser
+from core.armour.armour_parser import ArmourParser
+from core.parser import TableParser
 
 def main():
     """Main function of the program."""
-    main_scraper: Scraper = Scraper("https://www.google.com")
-    html = main_scraper.scrape_page()
+    main_scraper: Scraper = Scraper("https://valheim.fandom.com/wiki/Armor")
 
-    main_parser: Parser = Parser(html)
-    print(main_parser.get_soup_prettyfied())
+    try :
+        html: str = main_scraper.scrape_page()
+
+        armour_tables: list = []
+        
+        capes_table: list = []
+        helmets_table: list = []
+        chest_table: list = []
+        legs_table: list = []
+
+        table_parser: TableParser = TableParser(html)
+
+        armour_parser: ArmourParser = ArmourParser(table_parser, capes_table, helmets_table, chest_table, legs_table)
+
+        print(armour_parser.parse_capes())
+
+    except TypeError as type_error:
+        print(type_error)
+
 
 if __name__ == "__main__":
     main()

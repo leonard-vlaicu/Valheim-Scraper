3bee1c26216d51f642cf9592f56394aaa6177dde
leonard-vlaicu <vlaicu.leonard@yahoo.com>
2023-08-26 02:12:43 +0300

    added generic TableParser

--- src/core/Parser.py
+++ src/core/Parser.py
@@ -13,3 +13,21 @@ class Parser:
     def get_soup_prettyfied(self):
         """returns the soup object prettyfied"""
         return self.soup.prettify()
+
+class TableParser:
+    """Parser class that parses the html content for tables"""
+    def __init__(self, html_content: str):
+        self.soup = BeautifulSoup(html_content, 'html.parser')
+        self.tables = self.soup.find_all('table')
+
+    def get_table_by_index(self, index: int):
+        """returns the table by index"""
+        return self.tables[index]
+
+    def get_tr_by_index(self, index: int):
+        """returns the tr by table index"""
+        return self.tables[index].find_all('tr')
+
+    def get_tables(self):
+        """returns the tables"""
+        return self.tables
